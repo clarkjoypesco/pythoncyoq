c969f9db88e5d3d8ee8a9476c0614ca65d091a96
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-04-13 16:51:46 +0800

    added medium difficulty

--- cyoq.py
+++ cyoq.py
@@ -1,6 +1,7 @@
 # Game has 3 or 4 levels
 # Immediately after running the program, user is prompted to select a difficulty level from easy / medium / hard
 lives = 3
+score = 0
 difficulty = [1, 2, 3]
 levels_list = ['Level 1', 'Level 2', 'Level 3', 'Level 4']
 sublevel_list = 4
@@ -32,9 +33,34 @@ if user_input == 'easy':
        
         if answer_input == answers_list[0][index]:
             
-            print 'Your Answer: ' + str(answers_list[0][index]) + ' is correct!'
+            print 'Your Answer: ' + str(answer_input) + ' is correct!'
             index +=1
+            score +=1
+            
         else:
+            print 'Your Answer: ' + str(answer_input) + ' is incorrect!'
+            print 'Please try again!'
+            lives -=1
+            if lives <= 0:
+                print 'Game Over!'
+                break
+            print 'You have ' + str(lives) + ' lives left'
+
+if user_input == 'medium' or score==3:
+    index = 0
+    question_list = questions_list[index]
+    answer_list = answers_list[index]
+    while index <  len(question_list):
+        answer_input =  raw_input(questions_list[1][index] + '\n')
+       
+        if answer_input == answers_list[1][index]:
+            
+            print 'Your Answer: ' + str(answer_input) + ' is correct!'
+            index +=1
+            score +=1
+        else:
+            print 'Your Answer: ' + str(answer_input) + ' is incorrect!'
+            print 'Please try again!'
             lives -=1
             if lives <= 0:
                 print 'Game Over!'
